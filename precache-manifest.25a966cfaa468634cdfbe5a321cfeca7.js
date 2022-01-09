8be9e7a0ea4f3d5bc8f3058ff1c53ca332ca34f4
mashagrigorian <mashagrigorian5@gmail.com>
2022-01-09 20:38:41 +0400

    Updates

---
+++ precache-manifest.25a966cfaa468634cdfbe5a321cfeca7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36cf2c0be2f827382538133c9e8ff92f",
+    "url": "/mashagrigorian/demo/index.html"
+  },
+  {
+    "revision": "4ab7985b4ef6141c0a60",
+    "url": "/mashagrigorian/demo/static/css/4.e90bee16.chunk.css"
+  },
+  {
+    "revision": "761013f9eadbcdd0129e",
+    "url": "/mashagrigorian/demo/static/css/5.fc32f28d.chunk.css"
+  },
+  {
+    "revision": "55ee8569a436faa0b259",
+    "url": "/mashagrigorian/demo/static/css/6.e9157d59.chunk.css"
+  },
+  {
+    "revision": "090fe1fb94f5d0dbb55d",
+    "url": "/mashagrigorian/demo/static/css/7.62de54b2.chunk.css"
+  },
+  {
+    "revision": "7a8161e7d03ac56518fc",
+    "url": "/mashagrigorian/demo/static/css/main.4f65d9a9.chunk.css"
+  },
+  {
+    "revision": "3899eb7d3d9b8e97b2bd",
+    "url": "/mashagrigorian/demo/static/js/0.1fa55b18.chunk.js"
+  },
+  {
+    "revision": "0b6d5d22b2bb47dfaaa4",
+    "url": "/mashagrigorian/demo/static/js/3.61fe266e.chunk.js"
+  },
+  {
+    "revision": "4ab7985b4ef6141c0a60",
+    "url": "/mashagrigorian/demo/static/js/4.3128ad5b.chunk.js"
+  },
+  {
+    "revision": "761013f9eadbcdd0129e",
+    "url": "/mashagrigorian/demo/static/js/5.2b051fe3.chunk.js"
+  },
+  {
+    "revision": "55ee8569a436faa0b259",
+    "url": "/mashagrigorian/demo/static/js/6.c2137adf.chunk.js"
+  },
+  {
+    "revision": "090fe1fb94f5d0dbb55d",
+    "url": "/mashagrigorian/demo/static/js/7.aecf3609.chunk.js"
+  },
+  {
+    "revision": "67b036f388986c349025",
+    "url": "/mashagrigorian/demo/static/js/8.3f45d768.chunk.js"
+  },
+  {
+    "revision": "7a8161e7d03ac56518fc",
+    "url": "/mashagrigorian/demo/static/js/main.ca221bc0.chunk.js"
+  },
+  {
+    "revision": "4b24f0a42c06281af73d",
+    "url": "/mashagrigorian/demo/static/js/runtime~main.6610b7e0.js"
+  },
+  {
+    "revision": "3f2590067056b4f0630d5b360e694fce",
+    "url": "/mashagrigorian/demo/static/media/preloader.3f259006.gif"
+  },
+  {
+    "revision": "b1c131b8da9278bff3be80106c7d2058",
+    "url": "/mashagrigorian/demo/static/media/user.b1c131b8.png"
+  }
+]);
